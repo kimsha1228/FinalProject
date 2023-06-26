07c04de1cebb6a1653ff18b4c1fdd7fd1f36043e
Roh <zxasqwsz@naver.com>
2023-06-26 20:57:48 +0900

    error fix

--- gittest/src/main/java/test/com/git/HomeController.java
+++ gittest/src/main/java/test/com/git/HomeController.java
@@ -11,10 +11,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Handles requests for the application home page.
  */
-@Sl4fj
+@Slf4j
 @Controller
 public class HomeController {
 
