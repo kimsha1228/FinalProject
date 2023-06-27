8880292318a157e89b223a2c884a69bf734170d6
kimsha1228 <tae8939@naver.com>
2023-06-27 11:49:06 +0900

    update cotroller commit

--- gittest/src/main/java/test/com/git/reservation/ReservationController.java
+++ gittest/src/main/java/test/com/git/reservation/ReservationController.java
@@ -17,43 +17,43 @@ public class ReservationController {
 	@Autowired
 	ReservationService service;
 	
-	@RequestMapping(value = "/rest_api.do", method = RequestMethod.GET)
-	public String rest_api() {
-		log.info("/rest_api.do");
-
-		return "rest_api";
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/json_mv_selectAll.do", method = RequestMethod.GET)
-	public List<ReservationVO> json_mv_selectAll() {
-		log.info("/json_mv_selectAll.do");
-		
-		List<ReservationVO> vos = service.selectAll();
-		
-		return vos;
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/json_mv_searchList.do", method = RequestMethod.GET)
-	public List<ReservationVO> json_mv_searchList(String searchKey, String searchWord) {
-		log.info("/json_mv_searchList.do");
-		log.info("searchKey:{}", searchKey);
-		log.info("searchWord:{}", searchWord);
-		
-		List<ReservationVO> vos = service.searchList(searchKey, searchWord);
-		
-		return vos;
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/json_mv_selectOne.do", method = RequestMethod.GET)
-	public ReservationVO json_mv_selectOne(ReservationVO vo) {
-		log.info("/json_mv_selectOne.do...{}",vo);
-		
-		ReservationVO vo2 = service.selectOne(vo);
-		if(vo2==null) vo2 = vo;
-		return vo2;
-	}
+//	@RequestMapping(value = "/rest_api.do", method = RequestMethod.GET)
+//	public String rest_api() {
+//		log.info("/rest_api.do");
+//
+//		return "rest_api";
+//	}
+//	
+//	@ResponseBody
+//	@RequestMapping(value = "/json_mv_selectAll.do", method = RequestMethod.GET)
+//	public List<ReservationVO> json_mv_selectAll() {
+//		log.info("/json_mv_selectAll.do");
+//		
+//		List<ReservationVO> vos = service.selectAll();
+//		
+//		return vos;
+//	}
+//	
+//	@ResponseBody
+//	@RequestMapping(value = "/json_mv_searchList.do", method = RequestMethod.GET)
+//	public List<ReservationVO> json_mv_searchList(String searchKey, String searchWord) {
+//		log.info("/json_mv_searchList.do");
+//		log.info("searchKey:{}", searchKey);
+//		log.info("searchWord:{}", searchWord);
+//		
+//		List<ReservationVO> vos = service.searchList(searchKey, searchWord);
+//		
+//		return vos;
+//	}
+//	
+//	@ResponseBody
+//	@RequestMapping(value = "/json_mv_selectOne.do", method = RequestMethod.GET)
+//	public ReservationVO json_mv_selectOne(ReservationVO vo) {
+//		log.info("/json_mv_selectOne.do...{}",vo);
+//		
+//		ReservationVO vo2 = service.selectOne(vo);
+//		if(vo2==null) vo2 = vo;
+//		return vo2;
+//	}
 
 }
