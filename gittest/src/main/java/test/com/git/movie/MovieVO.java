18d16563946674ee4e7d347164da9131ce700098
Roh <zxasqwsz@naver.com>
2023-06-27 09:54:19 +0900

    클라우드 서버에 접속해보는 테스트. oracle.properties는 gitignore로 제외함

---
+++ gittest/src/main/java/test/com/git/movie/MovieVO.java
@@ -0,0 +1,16 @@
+package test.com.git.movie;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Data;
+
+@Data
+public class MovieVO {
+
+	private int num;
+	private String title;
+	private int price;
+	private String producer;
+	private String save_name;
+	private MultipartFile file;
+}
---
+++ gittest/src/main/webapp/WEB-INF/views/top_menu.jsp
@@ -0,0 +1,7 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<ul>
+	<li><a href="home.do">HOME</a></li>
+
+	<li><a href="rest_api.do">REST_API</a></li>
+</ul>
