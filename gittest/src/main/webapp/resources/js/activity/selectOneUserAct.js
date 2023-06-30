6b3e42c08b2e296dad5dd088424d929736e7c09c
Roh <zxasqwsz@naver.com>
2023-06-30 11:19:37 +0900

    selectOneUserAct 대략적으로 완성

--- gittest/src/main/webapp/resources/js/activity/selectOneUserAct.js
+++ gittest/src/main/webapp/resources/js/activity/selectOneUserAct.js
@@ -3,6 +3,7 @@ const urlParams = new URL(location.href).searchParams;
 let id = urlParams.get('id');
 console.log(id);
 
+//onload...
 $(function(){
 
 	//현재 상품의 이미지들 출력
@@ -35,7 +36,6 @@ $(function(){
 	});//end $.ajax()...
 	
 	//후기 한개 출력용
-	/*
 	$.ajax({
 		url : "json_selectOneComment.do",
 		data:{act_id:id},
@@ -61,10 +61,20 @@ $(function(){
 		},
 		error:function(xhr,status,error){
 			console.log('xhr.status:', xhr.status);
+			$('#OneComment').html('<p>댓글 불러오기 실패</p>');
 		}
 	});//end $.ajax()...
-	*/
+	
+	
+	
+	//날짜 선택을 오늘로 변경
+	var now = new Date();
+	now.setMinutes(now.getMinutes() - now.getTimezoneOffset()); //한국시간으로 변경
+	document.getElementById('datePicker').value = now.toISOString().slice(0, -14); //자릿수짜르기
+	
 });
+//end onload
+
 
 //클릭시 현재 주소를 복사 
 //https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/write 참조함
@@ -101,4 +111,17 @@ function addWish(user_id){
 			console.log('xhr.status:', xhr.status);
 		}
 	});//end $.ajax()...
+}
+
+function incrementQuantity() {
+	var quantityInput = document.getElementById('quantity');
+	var currentQuantity = parseInt(quantityInput.value);
+	quantityInput.value = currentQuantity + 1;
+}
+function decrementQuantity() {
+	var quantityInput = document.getElementById('quantity');
+	var currentQuantity = parseInt(quantityInput.value);
+	if (currentQuantity > 0) {
+		quantityInput.value = currentQuantity - 1;
+	}
 }
