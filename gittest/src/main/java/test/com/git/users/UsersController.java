2e92e03f7ca2a020c09053e5db5bb1d9ab3a4251
kimsha1228 <tae8939@naver.com>
2023-06-30 14:00:03 +0900

    연습커밋

--- gittest/src/main/java/test/com/git/users/UsersController.java
+++ gittest/src/main/java/test/com/git/users/UsersController.java
@@ -38,7 +38,7 @@ public class UsersController {
 	@ResponseBody
 	@RequestMapping(value = "/jsonUsersSelectOne.do", method = RequestMethod.GET)
 	public UsersVO jsonUsersSelectOne(UsersVO vo) {
-		log.info("jsonUserSelectOne.do...{}", vo);
+		log.info("jsonUserSelectOne.do...연습하는 중....{}", vo);
 		
 		UsersVO vo2 = service.selectOne(vo);
 		log.info("vo2: {}", vo2);
