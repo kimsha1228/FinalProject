d6101db624c4326033b2ee23162574f40512e7b9
yi <yi@59.17.70.161>
2023-07-05 11:41:28 +0900

    answerjs, jsp

--- gittest/src/main/webapp/resources/js/answer/insertAnswerOK.js
+++ gittest/src/main/webapp/resources/js/answer/insertAnswerOK.js
@@ -1,3 +1,22 @@
-/**
- * 
- */
+window.onload = function(){
+insertAnswer(1);
+};//end onload
+
+function insertAnswer(value){
+
+	$.ajax({
+		url:"jsonSelectAllAct.do",
+		data:{
+			user_id:user_id,
+			act_name: value
+		},
+		dataType:'json',
+		success:function(response){
+			console.log('ajax...success:',response);
+	
+		},
+	 	error:function(xhr,status,error){
+			console.log('xhr.status:',xhr.status);
+	 	}
+	});//end ajax
+};//end insertAnswer()
