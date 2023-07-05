bf7d1be53e62df974019f99c4a5ab281f3ad2cc2
kimsha1228 <tae8939@naver.com>
2023-07-06 00:14:18 +0900

    merge fix...

--- gittest/src/main/java/balgil/com/trip/cart/controller/CartController.java
+++ gittest/src/main/java/balgil/com/trip/cart/controller/CartController.java
@@ -7,11 +7,9 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import balgil.com.trip.activity.model.ActivityVO;
 import balgil.com.trip.activity.service.ActivityService;
