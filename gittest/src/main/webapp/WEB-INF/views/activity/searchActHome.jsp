df1cf7282c432c2f77aed520b1061be7db7ab064
Roh <zxasqwsz@naver.com>
2023-07-13 11:31:30 +0900

    searchActHome에 테이블적용

--- gittest/src/main/webapp/WEB-INF/views/activity/searchActHome.jsp
+++ gittest/src/main/webapp/WEB-INF/views/activity/searchActHome.jsp
@@ -4,10 +4,20 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" >
+<link rel="stylesheet" href="resources/css/tablesorter/theme.default.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script type="text/javascript" src="resources/js/route/tablesorter/jquery.tablesorter.min.js"></script>
+<script src="https://mottie.github.io/tablesorter/addons/pager/jquery.tablesorter.pager.js"></script>
+
 <title>홈 상품 검색</title>
-<script
-	src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-<script type="text/javascript" src="resources/js/activity/searchActHome.js"></script>
+<script>
+    let user_id = '<%= session.getAttribute("user_id") %>';
+    console.log("현재 로그인 되어있는 아이디:", user_id);
+</script>
+<script type="text/javascript" src="resources/js/activity/searchActHome.js?ver=2"></script>
 </head>
 <body>
 	<jsp:include page="../top_menu.jsp"></jsp:include>
@@ -20,30 +30,43 @@
 	<input type="text" name="searchWord" id="searchWord">
 	<button class="myButton" onclick="searchList()" >검색</button>
 
-	<table>
+	<table id="tableContainer">
 	<thead>
-		<tr>
-			<th>id</th>
-			<th>act_name</th>
-			<th>content</th>
-			<th>rate</th>
-			<th>vcount</th>
-			<th>tag</th>
-			<th>add</th>
-			<th>price</th>
-			<th>act_date</th>
-			<th>seller_id</th>
-			<th>dest_id</th>
-		</tr>
+        <tr>
+            <th class="sorter-false">id</th>
+            <th class="sorter-false">act_name</th>
+            <th class="sorter-false">rate</th>
+            <th class="sorter-false">price</th>
+            <th class="sorter-false">wishList</th>
+            <th class="sorter-false" style="display: none;">vcount</th>
+            <th class="sorter-false" style="display: none;">act_date</th>
+        </tr>
 	</thead>
 	<tbody id="vos">
 		
 	</tbody>
 	<tfoot>
-		<tr>
-			<td colspan="5">1 2 3 4 5</td>
-		</tr>
 	</tfoot>
-	</table>
+</table>
+<!-- 페이징 요소-->
+<div class="tableContainer">
+	<form>
+		<i class="fa-sharp fa-solid fa-angles-left fa-2xl first"></i>
+		<i class="fa-sharp fa-solid fa-angle-left fa-2xl prev"></i>
+		<!-- the "pagedisplay" can be any element, including an input -->
+		<span class="pagedisplay" data-pager-output-filtered="{startRow:input} &ndash; {endRow} / {filteredRows} of {totalRows} total rows"></span>
+		<i class="fa-sharp fa-solid fa-angle-right fa-2xl next"></i>
+		<i class="fa-sharp fa-solid fa-angles-right fa-2xl last"></i>
+<!-- 		<select class="pagesize"> -->
+<!-- 		<option value="10">10</option> -->
+<!-- 		<option value="all">All Rows</option> -->
+<!-- 		</select> -->
+	</form>
+</div>
+<!-- BootStrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" ></script>
+<script src="https://kit.fontawesome.com/0007d685f8.js" crossorigin="anonymous"></script>
+<!-- https://fontawesome.com/search 아이콘들 주소 -->
 </body>
 </html>
--- gittest/src/main/webapp/resources/js/activity/selectAllUserAct.js
+++ gittest/src/main/webapp/resources/js/activity/selectAllUserAct.js
@@ -70,8 +70,169 @@ $(function(){
 		}
 	});//end $.ajax()...
 	
+	callTableSorter();
 	
-	 // **********************************
+	
+	//위시리스트 추가 함수
+	$(document).on('click', '.wish', function() {
+	  let param_user_id = $(this).data('arg1');
+	  let param_act_id = $(this).data('arg2');
+	  let $wishelement = $(this);
+	
+	  console.log("insertWishListOk로 넘겨줄 파라미터", param_user_id, param_act_id);
+	
+	  $.ajax({
+	    url: "insertWishListOK.do",
+	    data: {
+	      act_id: param_act_id,
+	      user_id: param_user_id
+	    },
+	    method: 'POST',
+	    dataType: 'json',
+	    success: function(response) {
+	      console.log(response);
+	      if (response.result === 'OK') {
+	        alert("위시리스트에 추가했습니다");
+	        $wishelement.text('♥');
+	      } else {
+	        alert("위시리스트에 제거했습니다");
+	        $wishelement.text('♡');
+	      }
+	    },
+	    error: function(xhr, status, error) {
+	      console.log('xhr.status:', xhr.status);
+	    }
+	  });
+	});
+	
+	//엔터키만 눌러도 searchList가 작동
+	$('#searchWord').on('keydown', function(e) {
+	    var keyCode = e.which;
+	
+	    if (keyCode === 13) { // Enter Key
+	    	searchList();
+	    }
+	});
+	
+	
+});
+//end onload
+
+function searchList(){
+	
+	console.log("searchList()...",$('#searchKey').val(),$('#searchWord').val());
+	
+	$.ajax({
+		url:"jsonSearchAct.do",
+		data:{
+			searchKey:$('#searchKey').val(),
+			searchWord:$('#searchWord').val(),
+		},
+		method:'GET',
+		dataType:'json',
+		async : false,
+		success:function(response){
+			console.log('ajax...success:',response);
+			console.log(response.length);
+			let tag_vos='';
+			//루프돌리는방법1
+			for(let i in response){
+				let vo = response[i];
+				tag_vos+=`
+					<tr>
+ 					<td>				
+ 						<a href="selectOneAct.do?id=${vo.id}">${vo.id}</a>
+ 					</td>
+ 					<td>${vo.act_name}</td>
+ 					<td>${vo.rate}</td>
+ 					<td>${vo.price}</td>
+ 					<td><button class="wish" data-act_id="${vo.id}" data-arg1='${user_id}' data-arg2= '${vo.id}' data-arg3=${i})>♡</button></td>
+ 				</tr>
+				`;
+			}
+			
+			$("#vos2").html(tag_vos);
+		},
+	 	error:function(xhr,status,error){
+			console.log('xhr.status:',xhr.status);
+	 	}
+	});//end ajax
+	
+	//다시 위시리스트 하트 빈하트 여부 확인
+	$.ajax({
+		url : "jsonselectAllWishList.do",
+		data:{
+			user_id:user_id
+		},
+		method:'GET',
+		dataType:'json',
+		async : false,
+		success : function(response) {
+			//사용자의 위시리스트 만큼 반복
+			for(let i in response){
+				let vo = response[i];
+				
+				//추천 리스트내의 요소에서 확인
+				for(let j=0;j<rowCount;j++){
+					if($(`.wish:eq(${j})`).attr('data-act_id')==vo.act_id){
+					    $(`.wish:eq(${j})`).text('♥');
+					}
+				}
+				
+				//테이블내의 요소에서 확인
+				for(let j=0;j<$("#vos2 tr").length;j++){
+					if($(`#vos2 tr:eq(${j}) button`).attr('data-act_id')==vo.act_id){
+					    $(`#vos2 tr:eq(${j}) button`).text('♥');
+					}
+				}
+			}
+		},
+		error:function(xhr,status,error){
+			console.log('xhr.status:', xhr.status);
+		}
+	});//end $.ajax()...
+	
+};//end searchList()
+
+
+//참고한 사이트 https://mottie.github.io/tablesorter/docs/example-trigger-sort.html
+function sortFunction(value){
+	switch (value) {
+		//최근 등록
+		case 1:
+ 			$("#tableContainer").trigger("sorton", [ [[6,1]] ]);
+			break;
+			
+		//오래된 순
+		case 2:
+ 			$("#tableContainer").trigger("sorton", [ [[6,0]] ]);
+			break;
+
+		//조회순
+		case 3:
+			$("#tableContainer").find("th:contains(vcount)").trigger("sort");
+			$("#tableContainer").trigger("sorton", [ [[5,1]] ]);
+			break;
+		
+		//평점순
+		case 4:
+ 			$("#tableContainer").find("th:contains(rate)").trigger("sort");
+ 			$("#tableContainer").trigger("sorton", [ [[2,1]] ]);
+ 			break;
+ 			
+ 		//정렬 초기화
+ 		case 5:
+ 			$('#searchWord').val('');
+ 			searchList();
+ 			callTableSorter();
+ 		break;
+		default:
+    	console.log(`error sorting.`);
+	}
+}
+
+function callTableSorter(){
+ // **********************************
 	 //  tablesorter 페이징 옵션 변수
 	 // **********************************
 	  var pagerOptions = {
@@ -94,7 +255,7 @@ $(function(){
 	    page: 0,
 	
 	    // Number of visible rows - default is 10
-	    size: 10,
+	    size: 5,
 	
 	    // Save pager page & size if the storage script is loaded (requires $.tablesorter.storage in jquery.tablesorter.widgets.js)
 	    savePages : true,
@@ -154,165 +315,4 @@ $(function(){
     // initialize the pager plugin
     // ****************************
     .tablesorterPager(pagerOptions);
-	
-	//위시리스트 추가 함수
-	$('.wish').click(function(){
-		let param_user_id=$(this).data('arg1');
-		let param_act_id=$(this).data('arg2');
-		let $wishelement=$(this); //클릭한
-	//$(this).text()=='♡'?$(this).text('♥'):$(this).text('♡');
-		console.log("insertWishListOk로 넘겨줄 파라미터",param_user_id,param_act_id);
-		$.ajax({
-			url : "insertWishListOK.do",
-			data:{
-				act_id:param_act_id,
-				user_id:param_user_id
-			},
-			method:'POST',
-			dataType:'json',
-			success : function(response) {
-				console.log(response);
-				if(response.result==='OK'){
-					alert("위시리스트에 추가했습니다");
-					$wishelement.text('♥');
-				}else{
-					alert("위시리스트에 제거했습니다");
-					$wishelement.text('♡');
-				}
-			},
-			error:function(xhr,status,error){
-				console.log('xhr.status:', xhr.status);
-			}
-		});//end $.ajax()...
-		
-	});
-	
-	//엔터키만 눌러도 searchList가 작동
-	$('#searchWord').on('keydown', function(e) {
-	    var keyCode = e.which;
-	
-	    if (keyCode === 13) { // Enter Key
-	    	searchList();
-	    }
-	});
-	
-	
-});
-//end onload
-
-function searchList(){
-	
-	console.log("searchList()...",$('#searchKey').val(),$('#searchWord').val());
-	
-	$.ajax({
-		url:"jsonSearchAct.do",
-		data:{
-			searchKey:$('#searchKey').val(),
-			searchWord:$('#searchWord').val(),
-		},
-		method:'GET',
-		dataType:'json',
-		success:function(response){
-			console.log('ajax...success:',response);
-			console.log(response.length);
-			let tag_vos='';
-			//루프돌리는방법1
-			for(let i in response){
-				let vo = response[i];
-				tag_vos+=`
-					<tr>
- 					<td>				
- 						<a href="selectOneAct.do?id=${vo.id}">${vo.id}</a>
- 					</td>
- 					<td>${vo.act_name}</td>
- 					<td>${vo.rate}</td>
- 					<td>${vo.price}</td>
- 					<td><button id="wish${i}" class="${vo.id}" onclick="addWish('${user_id}','${vo.id}',${i})">♡</button></td>
- 				</tr>
-				`;
-			}
-			
-			$("#vos2").html(tag_vos);
-		},
-	 	error:function(xhr,status,error){
-			console.log('xhr.status:',xhr.status);
-	 	}
-	});//end ajax
-};//end searchList()
-
-
-
-// 테이블 클릭할때
-$('#DataTables_Table_0_paginate').click(function(){
-    
-    //버튼을 클릭할떄
-    $('#vos2 button').click(function(){
-    console.log('haha5');
-	});
-	
-	$.ajax({
-		url : "jsonselectAllWishList.do",
-		data:{
-			user_id:user_id
-		},
-		method:'GET',
-		dataType:'json',
-		async : false,
-		success : function(response) {
-			//사용자의 위시리스트 만큼 반복
-			console.log(rowCount);
-			for(let i in response){
-				let vo = response[i];
-						console.log("확인1");
-						console.log(vo);
-						console.log(vo.act_id);
-				//페이지내의 요소에서 확인
-				for(let j=0;j<rowCount;j++){
-					if($(`.wish:eq(${j})`).attr('data-act_id')==vo.act_id){
-						console.log("확인2");
-					    $(`.wish:eq(${j})`).text('♥');
-					}
-				}
-			}
-		},
-		error:function(xhr,status,error){
-			console.log('xhr.status:', xhr.status);
-		}
-	});//end $.ajax()...
-	
-	
-});
-
-//참고한 사이트 https://mottie.github.io/tablesorter/docs/example-trigger-sort.html
-function sortFunction(value){
-	switch (value) {
-		//최근 등록
-		case 1:
- 			$("#tableContainer").trigger("sorton", [ [[6,1]] ]);
-			break;
-			
-		//오래된 순
-		case 2:
- 			$("#tableContainer").trigger("sorton", [ [[6,0]] ]);
-			break;
-
-		//조회순
-		case 3:
-			$("#tableContainer").find("th:contains(vcount)").trigger("sort");
-			$("#tableContainer").trigger("sorton", [ [[5,1]] ]);
-			break;
-		
-		//평점순
-		case 4:
- 			$("#tableContainer").find("th:contains(rate)").trigger("sort");
- 			$("#tableContainer").trigger("sorton", [ [[2,1]] ]);
- 			break;
- 			
- 		//정렬 초기화
- 		case 5:
- 			$("#tableContainer").trigger("sortReset");
- 		break;
-		default:
-    	console.log(`error sorting.`);
-	}
 }
